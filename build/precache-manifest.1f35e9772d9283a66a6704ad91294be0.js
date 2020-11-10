b84471bd70ca27ef25f7c41279a60cc49343ec13
ShriyaS-create <shyamshriya@gmail.com>
2020-11-10 11:25:56 +0530

    final code

---
+++ build/precache-manifest.1f35e9772d9283a66a6704ad91294be0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "128c2360a247cb4686858117fd3a7b5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63d96dc3f376b84d673a",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "096870dc46339c0b1974",
+    "url": "/static/css/main.f9890a37.chunk.css"
+  },
+  {
+    "revision": "63d96dc3f376b84d673a",
+    "url": "/static/js/2.2a45f9d5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2a45f9d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "096870dc46339c0b1974",
+    "url": "/static/js/main.f0a78caa.chunk.js"
+  },
+  {
+    "revision": "b957cec06c6437b7ad73",
+    "url": "/static/js/runtime-main.c7ceb169.js"
+  }
+]);
